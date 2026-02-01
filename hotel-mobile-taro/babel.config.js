9fb2dc1c21417a7e1e64c4589155a93ed5df9675
lwayne7 <wanglang@shu.edu.cn>
2026-02-01 21:45:52 +0800

    chore: 移除 Taro 子目录（已迁至独立仓库 hotel-mobile-taro）

--- hotel-mobile-taro/babel.config.js
+++
@@ -1,9 +0,0 @@
-// babel-preset-taro 更多选项： https://github.com/NervJS/taro/tree/next/packages/babel-preset-taro
-module.exports = {
-  presets: [
-    ['taro', {
-      framework: 'react',
-      ts: true,
-    }],
-  ],
-};
--- hotel-mobile-taro/src/app.tsx
+++
@@ -1,7 +0,0 @@
-import './app.scss';
-
-function App(props) {
-  return props.children;
-}
-
-export default App;
